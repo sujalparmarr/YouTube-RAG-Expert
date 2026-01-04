cd4afc8e4df6269ff95238bd8fe31b9a8056dfa2
Sujal Parmar <119657662+sujalparmarr@users.noreply.github.com>
2026-01-04 23:49:13 +0530

    small fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit import spinner
 from streamlit.web.server.server import server_port_is_manually_set
-
+ 
 from supporting_functions import (
      extract_video_id,
      get_transcript,
